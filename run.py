fbf21469d64909e91bf96e4e12218c8768f112b2
tjasajan <33231726+tjasajan@users.noreply.github.com>
2018-04-10 22:08:57 +0200

    fix username check, display online users, add logout

--- run.py
+++ run.py
@@ -6,12 +6,13 @@ import os
 app = Flask(__name__)
 app.secret_key = 'some_secret_key'
 
+online_users = []
 
 def check_if_username_already_exists(selected_username):
     # Check if username already exists
-    # NEED FIX: PARTIAL WORDS
     with open('data/users.txt', 'r') as users_file:
-        if selected_username in users_file.read():
+        users_list = [line.rstrip() for line in users_file]
+        if selected_username in users_list:
             return True
         else:
             return False
@@ -32,6 +33,8 @@ def register_user():
             # Add username to users.txt and redirect to game
             with open('data/users.txt', 'a') as users:
                 users.write(selected_username + '\n')
+            # Add user to online users
+            online_users.append(selected_username)
             return redirect(selected_username)         
     return render_template('register.html')
     
@@ -40,19 +43,26 @@ def login_user():
     if request.method == 'POST':
         selected_username = request.form['username']
         if check_if_username_already_exists(selected_username):
+            # Add user to online users
+            online_users.append(selected_username)
             # Redirect to game if username OK
             return redirect(selected_username)
         else:
             # Display message for wrong username
             error_wrong_username = 'Wrong username, please try again'
             return render_template('login.html', message=error_wrong_username)
-        
     return render_template('login.html')
 
 @app.route('/<username>')
 def user_play_game(username):
-    return render_template('game.html', username=username)
+    
+    return render_template('game.html', username=username, online_users=online_users)
 
+@app.route('/logout/<username>')
+def logout_user(username):
+    # Remove user from online users and redirect to home page
+    online_users.remove(username)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
