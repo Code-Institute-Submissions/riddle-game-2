d193489ff8780b5017943c990a0e8e5c8a32ee40
tjasajan <33231726+tjasajan@users.noreply.github.com>
2018-04-11 20:23:44 +0200

    ask questions, show wrong answers

--- run.py
+++ run.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template, redirect, request
 import os
-# import json
+import json
+import random
 # from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'some_secret_key'
 
 online_users = []
+wrong_answers = []
+
+# random_index_list = []
+# def get_random_index_list():
+#     # Get list with random numbers for riddle index
+#     numbers_in_range = list(range(0, 20))
+#     random_number_list = random.sample(numbers_in_range, 20)
+#     for number in random_number_list:
+#         random_index_list.append(number)
+
 
 def check_if_username_already_exists(selected_username):
     # Check if username already exists
@@ -53,10 +64,30 @@ def login_user():
             return render_template('login.html', message=error_wrong_username)
     return render_template('login.html')
 
-@app.route('/<username>')
-def user_play_game(username):
+@app.route('/<username>', methods=['GET', 'POST'])
+def user_play_game(username):   
+    riddles_data = []
     
-    return render_template('game.html', username=username, online_users=online_users)
+    # Load data from riddles.json
+    with open('data/riddles.json', 'r') as json_data:
+        riddles_data = json.load(json_data)
+    
+    riddle_index = 0
+    if request.method == 'POST':
+        answer = request.form['answer'].lower()
+        # Get current index from template
+        riddle_index = int(request.form['riddle_index'])
+        # If correct answer and not the last riddle, go to next riddle, else add answer to wrong answers
+        if answer == riddles_data[riddle_index]['answer'].lower():
+            if riddle_index < len(riddles_data) - 1:
+                riddle_index += 1
+            else:
+                # After last riddle go to page with results
+                return render_template('end_game_results.html')
+        else:
+            wrong_answers.append(answer)
+    
+    return render_template('game.html', username=username, online_users=online_users, riddles=riddles_data, riddle_index=riddle_index, wrong_answers=wrong_answers)
 
 @app.route('/logout/<username>')
 def logout_user(username):
