af761e87c1a11660d4666c9a0858725ef860bd7b
tjasajan <33231726+tjasajan@users.noreply.github.com>
2018-04-23 16:57:41 +0200

    add tests

--- test_run.py
+++ test_run.py
@@ -21,3 +21,20 @@ class TestRiddle(unittest.TestCase):
         scores_sorted2 = test_if_scores_sorted(scores[7], scores[3])
         self.assertTrue(scores_sorted1)
         self.assertFalse(scores_sorted2)
+        
+    # Test if answers with different capitalization are accepted
+    def test_if_answer_is_accepted(self):
+        answer1 = run.answer_to_lowercase('KEYBOARD')
+        answer2 = run.answer_to_lowercase('KeYbOaRd')
+        riddle_answer = run.riddles_data[2]['answer'].lower()
+        self.assertEqual(answer1, riddle_answer)
+        self.assertEqual(answer2, riddle_answer)
+        
+    # Test if answer is a word - only alphabetic characters
+    def test_if_answer_is_a_word(self):
+        answer1 = run.answer_is_a_word('dfshajfh')
+        answer2 = run.answer_is_a_word('dsf3sdga')     
+        answer3 = run.answer_is_a_word('dfsasd-sf')
+        self.assertTrue(answer1)
+        self.assertFalse(answer2)
+        self.assertFalse(answer3)
