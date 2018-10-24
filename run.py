d660d1266497bd34e7f964d13d2351dd8dcf194b
TJASA <tjasaa@gmail.com>
2018-10-24 16:21:04 +0200

    disable debug mode

--- run.py
+++ run.py
@@ -145,4 +145,4 @@ def logout_user(username):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT', 5000)),
-            debug=True)
+            debug=False)
